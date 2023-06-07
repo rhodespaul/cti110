a174c8de7e54b92938bed73549fc8de5f08fbbba
rhodespaul <135713077+rhodespaul@users.noreply.github.com>
2023-06-07 09:25:39 -0400

    Add files via upload

---
+++ P1HW1_RhodesPaul.py
@@ -0,0 +1,15 @@
+user_num = int(input('Enter integer:\n'))
+sq_usernum = user_num*user_num
+cube_usernum = user_num*user_num*user_num
+print('You entered:', user_num)
+print(user_num, 'squared is', sq_usernum)
+print('And', user_num, 'cubed is', cube_usernum, '!!')
+user_num2 = int(input('Enter another integer:\n'))
+usernum_add = user_num+user_num2
+usernum_x = user_num*user_num2
+print(user_num, '+', user_num2, 'is', usernum_add)
+print(user_num, '*', user_num2, 'is', usernum_x)
+
+
+
+# Type your code here
