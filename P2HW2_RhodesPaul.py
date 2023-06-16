307051fe802ac3cbb9ef3f1352f93e065ece6efe
rhodespaul <135713077+rhodespaul@users.noreply.github.com>
2023-06-16 00:21:01 -0400

    Add files via upload
    Grades Calc

---
+++ P2HW2_RhodesPaul.py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Sythe/OneDrive/Documents/Assignments/CTI-110/P1HW1_RhodesPaul/test.py
+Enter grade for Module 1: 45
+Enter grade for Module 2: 77
+Enter grade for Module 3: 88
+Enter grade for Module 4: 99.6
+Enter grade for Module 5: 78.999
+Enter grade for Module 6: 76.8
+
+------------Results------------
+Lowest Grade:       45.0
+Highest Grade:      99.6
+Sum of Grades:      465.4
+Average:            61.00
+----------------------------------------
